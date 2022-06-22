41f60054f8d87f1693dc7b28c0b4b56197e44196
Potterf <ge45lep@mytum.de>
2022-06-22 16:36:14 +0200

    added new folder for input data

--- myPaths.R
+++ myPaths.R
@@ -6,6 +6,7 @@
 # Input paths
 myPath     = "C:/Users/ge45lep/Documents/2021_Franconia_mortality"
 inFolder   = "raw"
+inFolderFieldVeg = "fieldData/04_students_field/data/vegetation" # field data to store vegetation outputs
 outSpatial = "outSpatial"
 outTable   = "outTables"
 outReach   = "outReach"
