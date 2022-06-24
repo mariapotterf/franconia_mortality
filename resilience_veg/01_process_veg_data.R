7886e7ccc7327a8737f76c42b33f46bfd5952426
Potterf <ge45lep@mytum.de>
2022-06-24 14:42:35 +0200

    process vegetation data from the Survey123 ArcGIs. Involves: - create unique colnames, replace by ENG - export site (inner) characterictics - organize dtaset - make first graphics

--- resilience_veg/01_process_veg_data.R
+++ resilience_veg/01_process_veg_data.R
@@ -4,33 +4,220 @@
 # collected during field work 2022
 # by Juri and Sophia
 
-# To do:
+# Process: -----------------------------
+# Read the data by weeks
+# order the data by the columns by the german names: master name is Week 3 [ENG]
+# bin the data by rows
 
-# Read the data
-# ttranslate german names???
+# Output: ------------------------------
+# calculate the statistics per patches (relative to sample area) - sometimes 5 patches, sometimes 15,...
+# need to recalculate the values to hectare
+
+# read translation to ENG names : check if teh number of colums is table between the surveys? translate german names???
 #   to know what is what
+# process data: regenerations tree species by heights (HKX) 
 
-source('myPaths.R')
 
-# Loading
-library("readxl")
-
-# xlsx files
-dat <- read_excel(paste(myPath, inFolderFieldVeg, "Data Week 3.xlsx", sep = '/'))
-
-names(dat)
-
-# Translate:
-
+# Data structure:  
+# -----------------------------------
 # each record in the Table field has its own columns: ~ 600 columns
 # Regeneration: tree regeneration is split across tree species, height classes and damage
 # each tree species in height class: has 7 columns
 # 6 heights levels: 6*7 = 42 columns per species
 # 
 
-# get organized by the columns names:
-# to get some reasonable data: 
+# Colnames interpretation:
+# if 'env' - surroundings (environment)
+# if not env - in the site 
+# completed unique names for tree, species, etc.
+
+# Regeneration Tree species (12 species in total):
+# 
+#Spruce
+#Beech
+#Rowan
+#Fir
+#Oak
+#Sycamore
+#Birch
+#Willow
+#Pine
+#Ash
+#Other hardwood
+#Other softwood 
+
+reg_trees <- c(
+  'Spruce',
+  'Beech',
+  'Rowan',
+  'Fir',
+  'Oak',
+  'Sycamore',
+  'Birch',
+  'Willow',
+  'Pine',
+  'Ash',
+  'OtherHardwood',
+  'OtherSoftwood'
+)
+
+# height classes:
+reg_trees_heights <- c(
+  'HK1', # 0.2-0.4 m 
+  'HK2', # 0.4-0.6 m
+  'HK3', # 0.6-0.8 m
+  'HK4', # 0.8-1.0 m
+  'HK5', # 1.0-1.3 m
+  'HK6'  # 1.3-2.0 m
+)
 
 
-# How to read human readable format spread across columns?
- 
+# 
+
+
+source('myPaths.R')
+
+# Loading
+library(readxl)
+library(dplyr)
+library(data.table)
+library(tidyr)
+library(ggplot2)
+
+# read data 
+dat1  <- read_excel(paste(myPath, inFolderFieldVeg, "Data Week 3.xlsx", sep = '/'))
+dat2  <- read_excel(paste(myPath, inFolderFieldVeg, "Data_Week_1-2.xlsx", sep = '/'))
+
+# Read New headingsL: in ENG, with unique colnames
+# this was done manually in Excell
+EN_heading <- read_excel(paste(myPath, 
+                               inFolderFieldVeg, 
+                                "col_names_ENG.xlsx", sep = '/'), 
+                          sheet = "en_name")  # sheet name
+# check if teh colnames are equal??
+names(dat1) == names(dat2)
+
+# If the names are the same, we can bind them together
+
+# check in excel??
+#EN_heading_check <- read_excel(paste(myPath, 
+#                               inFolderFieldVeg, 
+#                               "col_names_ENG.xlsx", sep = '/'), 
+#                         sheet = "check_names_over_weeks")  # sheet name
+
+dat <- rbind(dat1, dat2)
+
+
+# Check if the columns lenght is the same:
+ncol(dat)         # 617
+nrow(EN_heading)  # 617
+
+# Remove the spaces from the En_heading, replace some not important characters
+EN_col_names <- gsub(' ', '_', EN_heading$ENG)
+EN_col_names <- gsub('_in_', '_', EN_col_names)
+EN_col_names <- gsub('\\.', '',     EN_col_names) # . means any character, so need to add \\
+
+
+# Replace the naming to have unique name for each colums 
+# and be able to filter throught them
+colnames(dat) <- EN_col_names
+
+# Get columsn for photos:
+photos_id <- c("North_subplot",
+               "east_subplot",
+               "north_environment",
+               "east_environment",
+               "south_environment",
+               "west_environment")
+
+
+# Get columss for the site identification
+plot_info <- c("ObjectID",
+               "GlobalID",
+               "triplet_no",
+               "triplet_type",
+               "surface_type",
+               "Subplot_no",
+               "gradient",
+               "exposure")
+
+# get columns for ground cover
+ground_cover <- c("Mature_Trees",
+                  "rejuvenation",
+                  "shrub_layer",
+                  "mosses",
+                  "ferns",
+                  "herb_layer",
+                  "grasses",
+                  "soil/foliage",
+                  "rock",
+                  "deadwood/stumps")
+
+# To get the richness per site:
+# get individual tables for each tree species, for each height
+# get counts 
+# start with 'Spruce'
+
+
+
+# -------------------------------------------------------
+#                       Regeneration
+# -------------------------------------------------------
+
+
+# select only columns with regeneration:
+# columns are in different class (logi, num, char), some contains NEIN: characters: need only counts!!!
+# if the count is not present, than it is 0
+
+# Sebset counts for regeneration datasets
+df_regen0 <- dat %>% 
+  dplyr::select(matches(paste(c("triplet_no","triplet_type","surface_type",'Subplot_no', reg_trees), collapse = '|'))) %>% 
+  dplyr::select(!matches("Number")) %>% 
+  mutate(uniqueID = paste(triplet_no,triplet_type,surface_type,Subplot_no, sep = '_' )) %>% 
+  dplyr::select(-c("triplet_no","triplet_type","surface_type",'Subplot_no')) %>%
+  dplyr::select_if(function(col) all(col == .$uniqueID) | is.numeric(col)) # select the numeric columns and the siteID (character)
+
+
+# convert from wide to long to calculate the counts by height classes:
+df_regen <- df_regen0 %>% 
+  pivot_longer(!uniqueID, names_to = 'type', values_to = 'count') %>% 
+  separate(type, c('species', 'height_class'), '_') %>% 
+  separate(uniqueID, c('trip_n', 'dom_sp', 'type', 'sub_n'), '_') #%>% 
+  #filter(count != 0)
+  #ggplot(aes(count, group = factor(height_class))) +
+  #geom_bar()
+
+
+
+# calculate richness:
+# count number of species per plot
+#richness <- 
+  df_regen %>% 
+  filter(count != 0) %>% # need to filter zero counts first
+  group_by(trip_n,dom_sp, type) %>% 
+  distinct(species) %>% 
+    count() %>% 
+    ggplot(aes(y = n,
+               x = type)) +
+    geom_boxplot() + 
+   # geom_jitter() +
+    facet_grid(~dom_sp) +
+    ylab('richness \n(# of tree species)')
+  
+  
+# What is regeneration height??
+df_regen %>% 
+  filter(count != 0)  %>% # need to filter zero counts first
+  group_by(trip_n,dom_sp, type) %>% 
+#  distinct(species) %>% 
+#  count() %>% 
+  ggplot(aes(y = count,
+               x = height_class,
+             fill = type)) +
+    geom_boxplot() + 
+    # geom_jitter() +
+    facet_grid(.~dom_sp) #+
+    #ylab('richness \n(# of tree species)')
+  
+  
+  
