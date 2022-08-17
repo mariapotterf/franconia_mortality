0dc93faa290bb215c52d2279985eb88aeeb99035
Potterf <ge45lep@mytum.de>
2022-08-17 11:52:23 +0200

    made a new script for common variables to read into script

---
+++ common_variables.R
@@ -0,0 +1,7 @@
+
+# Common variables:
+
+triplets_exclude = c(45, # oak, recently harvested 
+                     47 # oak, recently harvested
+                       # spruce - harvested dead Passau
+                    )
