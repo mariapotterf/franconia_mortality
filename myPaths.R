74e54d2410ac33ea471feef4d262402b929a4ff9
Potterf <ge45lep@mytum.de>
2022-08-18 11:57:13 +0200

    changed folder naming

--- myPaths.R
+++ myPaths.R
@@ -6,7 +6,7 @@
 # Input paths
 myPath     = "C:/Users/ge45lep/Documents/2021_Franconia_mortality"
 inFolder   = "raw"
-inFolderFieldVeg = "03_plot_sampling/students_field/data/vegetation" # field data to store vegetation outputs
+inFolderFieldVeg = "03_plot_sampling/out_fieldData/data/vegetation" # field data to store vegetation outputs
 outSpatial = "outSpatial"
 outTable   = "outTables"
 outReach   = "outReach"
