2145929c4f108aa0d692b3f92e8fcf233c5c1b5f
Potterf <ge45lep@mytum.de>
2022-08-18 11:57:24 +0200

    added plot variables

--- common_variables.R
+++ common_variables.R
@@ -1,7 +1,8 @@
 
 # Common variables:
 
-triplets_exclude = c(45, # oak, recently harvested 
+triplets_exclude = c(45, # no permission, no sampled
+                     65, # no sampled: no owner permission
                      47 # oak, recently harvested
                        # spruce - harvested dead Passau
                     )
