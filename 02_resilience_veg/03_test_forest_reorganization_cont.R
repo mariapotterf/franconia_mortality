0f91cc52dbd5bd74f0c327570db321c0e7a808ab
Potterf <ge45lep@mytum.de>
2022-10-21 09:10:18 +0200

    scatter plots

--- 02_resilience_veg/03_test_forest_reorganization_cont.R
+++ 02_resilience_veg/03_test_forest_reorganization_cont.R
@@ -19,6 +19,7 @@ library(tidyr)
 library(ggplot2)
 library(stringr)  # use regex expressions
 library(ggpubr)
+library(ggrepel)
 
 # set theme: plotting: 
 theme_set(theme_bw())
@@ -44,11 +45,10 @@ load(file = paste(getwd(), "outData/eco_traits.Rdata", sep = '/'))
 # Identify data to use:
 head(df_full_corr)        # - full PLOT based data: df_full_corr, seedlings, advanced, mature 
 head(plot_IVI)            # - df importance value:from plot, env mature, env advanced, merged by density/ha
-#head(df_winners)          # - novel species:        df_novelty
 head(trait_df)            # - trait values for all species: eco_traits
-#head(df_ground)           # - ground cover, in classes by 5%  
 head(df_mature_trees_env) # - trees in the surroundings: mature trees
 head(df_advanced_env)     # - trees in the surroundings: advanced
+
 
 # Master plots:
 head(plot_counts_df)      # - total count of the plots per triplets & categories: to standardize the densities...
@@ -172,7 +172,8 @@ df_RA1 <-
 p_RA1 <- df_RA1 %>% 
   ggplot(aes(RA1, fill = manag)) + 
   xlim(-5,5) +
-  dens_plot_details()
+  dens_plot_details() +
+  ggtitle('Dominant sp.')
   #geom_density(alpha = 0.8)
 
 # 
@@ -217,7 +218,8 @@ df_RA2 <-
 p_RA2 <-df_RA2 %>% 
   ggplot(aes(RA2, fill = manag)) +
   xlim(-3,3) +
-  dens_plot_details()
+  dens_plot_details()+
+  ggtitle('Richness')
 
 
 
@@ -253,7 +255,8 @@ df_RA3 <-
 p_RA3 <- df_RA3 %>% 
   ggplot(aes(RA3, fill = manag)) +
   xlim(-1.5,1.5) +
-  dens_plot_details()
+  dens_plot_details() +
+  ggtitle('Shade tolerance')
 
 
 
@@ -281,7 +284,8 @@ df_RS1 <-
 p_RS1 <- df_RS1 %>% 
   ggplot(aes(RS1, fill = manag)) +
   xlim(-15,15) +
-  dens_plot_details()
+  dens_plot_details()+
+  ggtitle('Stem density')
 
 
 
@@ -335,13 +339,96 @@ df_RS2 <-
 p_RS2 <- df_RS2 %>% 
   ggplot(aes(RS2, fill = manag)) +
   xlim(-3,3) +
-  dens_plot_details()
+  dens_plot_details()+
+  ggtitle('Horizontal str.')
 
 
+
+# Compare only horizontal distance to: ----------------------
+# - 1 mature trees:
+# - to advanced regen:
+# Make unique plots for: 
+# - all trees (PLOt + ENV), 
+# - only ENV 
+# - only the nearest PLOT or ENV?
+p_avg_distance_nearest <- df_full_corr %>% 
+  filter(count  != 0 ) %>% 
+  #filter(manag != 'l') %>%
+  filter(vert_layer != 'regen') %>% 
+  dplyr::select(trip_n, manag, sub_n, distance, height_class) %>%
+  mutate(distan_class = case_when(height_class %in% c("mature","mat_ENV") ~ 'mature',
+                                height_class %in% c("adv_ENV", "HK7")   ~ 'advanced')) %>% 
+  group_by(trip_n, manag, sub_n, distan_class) %>% # , height_class
+  slice(which.min(distance)) %>% # find teh closest mature tree: in plot or in ENV
+  full_join(plot_counts_df) %>% # add the 0 distances:! how to account if tree is missing??
+  ggplot(aes(x = factor(manag),
+             y = distance/100,
+             color = distan_class)) +
+  stat_summary() + 
+  scale_color_manual(name = "Height class", 
+                     breaks=c("mature", "advanced"),
+                     labels=c("mature", "advanced"),
+                     values = c("darkgreen","red")) +
+  ylab('Nearest tree\n Mature/advanced reg [m]') +
+  ggtitle('Nearest Mature/advanced reg [m]') +
+  ylim(0,6) +
+  theme(legend.position = 'bottom')
+
+
+# aveg distance all Mature:
+p_avg_distance_mature <- 
+  df_full_corr %>% 
+  filter(count  != 0 ) %>% 
+  filter(vert_layer != 'regen') %>% 
+  dplyr::select(trip_n, manag, sub_n, distance, height_class) %>%
+    filter(height_class %in% c("mature","mat_ENV")) %>% 
+  full_join(plot_counts_df) %>% # add the 0 distances:! how to account if tree is missing??
+  ggplot(aes(x = factor(manag),
+             y = distance/100,
+             color = height_class)) +
+  stat_summary(fun = mean) + 
+    scale_color_manual(name = "Location", 
+                       breaks=c("mat_ENV", "mature"),
+                       labels=c("ENV", "Plot"),
+                       values = c("red","black")) +
+  ylab('Mean distance to\nthe nearest Mature tree\n [m]') +
+  ylim(0,6)+
+  ggtitle('Mature tree') +
+  theme(legend.position = 'bottom') 
+
+
+
+
+# avg distance all advanced:
+p_avg_distance_adv <- 
+  df_full_corr %>% 
+    filter(count  != 0 ) %>% 
+    filter(vert_layer != 'regen') %>% 
+    dplyr::select(trip_n, manag, sub_n, distance, height_class) %>%
+    filter(height_class %in% c("HK7","adv_ENV")) %>%
+    full_join(plot_counts_df) %>% # add the missing trees:! how to account if tree is missing??
+    ggplot(aes(x = factor(manag),
+               y = distance/100,
+               color = height_class)) +
+    stat_summary(fun = mean) + 
+    scale_color_manual(name = "Location", 
+                       breaks=c("adv_ENV", "HK7"),
+                       labels=c("ENV", "Plot"),
+                       values = c("red","black")) +
+    ylab('Mean distance to\nthe nearest Advanced tree\n [m]') +
+  ylim(0,6) +
+  ggtitle('Advanced reg tree') +
+  theme(legend.position = 'bottom') 
 
   
 
+p_distances <- ggarrange(p_avg_distance_nearest, 
+                         p_avg_distance_adv,
+                         p_avg_distance_mature,
+                         nrow = 1, ncol = 3,
+                         hjust=-0.8)
 
+(p_distances)
 # RS3: Vertical structure -------------------------------------------------
 # 3 layers: 
 # - regeneration (<= less then 2 m height)
@@ -396,8 +483,27 @@ df_RS3[as.matrix(df_RS3) == Inf]  <- 0
 p_RS3 <- df_RS3 %>% 
   ggplot(aes(RS3, fill = manag)) +
   xlim(-4.3,4.3) +
-  dens_plot_details()
+  dens_plot_details() +
+  ggtitle('Vertical str.')
 
+
+
+
+
+# Plot densities together  ------------------------------------------------
+
+p_6vars <- ggarrange(
+  p_RA1 + ylim(0,1.5),
+  p_RA2 + ylim(0,1.5) ,
+  p_RA3 + ylim(0,1.5) ,
+  p_RS1 + ylim(0,1.5) ,
+  p_RS2 + ylim(0,1.5) ,
+  p_RS3 + ylim(0,1.5) ,
+  nrow = 2,
+  ncol = 3,
+  common.legend = TRUE,
+  legend = 'bottom'
+)
 
 # Join databases into one indicator by triplet -----------------------------
 # change is always indicated by the + values, - indicates the no change
@@ -422,8 +528,8 @@ out_reorg_pos[is.na(out_reorg_pos)] <- 0
 
 # merge indicators: -----------------------------------------
 out_reorg_pos <- out_reorg_pos %>% 
-  mutate(RA_mean = (RA1*RA2*RA3)/3,
-         RS_mean = (RS1*RS2*RS3)/3,
+  mutate(RA_mean = (RA1+RA2+RA3)/3,
+         RS_mean = (RS1+RS2+RS3)/3,
          RA_sum  = RA1+RA2+RA3,
          RS_sum  = RS1+RS2+RS3) 
 
@@ -439,26 +545,31 @@ out_reorg_pos %>%
 
 
 
-# plot averages:
-p_scatter_mean <- out_reorg_pos %>% 
+# plot averages: ---------------------------------
+p_scatter_mean <- 
+  out_reorg_pos %>% 
   left_join(trip_species) %>% 
   ggplot(aes(x = RA_mean,
              y = RS_mean,
-             color = dom_sp)) +
+             color = manag)) +
+  scale_color_manual(name = "Management", 
+                     breaks=c("c", "d"),
+                     labels=c("Clear-cut", "Dead"),
+                     values = c("red","black")) +
   geom_point() +
- # xlim(0,7) +
-#  ylim(0,7) +
+  xlim(0,2) +
+  ylim(0,2) +
   geom_abline(intercept = 0, # add diagnal line
               slope = c(0.5,2),
               col = "grey",
               size = .5,
               lty = 'dashed') +
-  facet_grid(manag~dom_sp) +
-  theme(legend.position = 'bottom') +
-  theme(aspect.ratio=1) # make plots perfect square
+  facet_grid(manag~dom_sp, scales = 'free') +
+  theme_update(legend.position = 'bottom') +
+  theme_update(aspect.ratio=1) # make plots perfect square
 
 
-
+p_scatter_mean
 
 
 
@@ -476,16 +587,17 @@ p_reorg_manag_dom_sp <- out_reorg_pos %>%
               size = .5,
               lty = 'dashed') +
   facet_grid(manag~dom_sp) +
-  theme(legend.position = 'bottom') +
-  theme(aspect.ratio=1) # make plots perfect square
+  theme_update(legend.position = 'bottom') +
+  theme_update(aspect.ratio=1) # make plots perfect square
 
 
 # plot sums: just two colors: ----------------------------------------
 windows()
-p_scatter_manag_sum <- out_reorg_pos %>% 
+#p_scatter_manag_sum <- 
+  out_reorg_pos %>% 
   left_join(trip_species) %>% 
-  ggplot(aes(x = RA_sum,
-             y = RS_sum,
+  ggplot(aes(x = RA_sum/3,
+             y = RS_sum/3,
              shape = manag,
              color = manag
              )) +
@@ -499,8 +611,8 @@ p_scatter_manag_sum <- out_reorg_pos %>%
                      labels=c("Clear-cut", "Dead"),
                      values = c(17, # triangle
                                 16)) + # circle
-  xlim(0,7) +
-  ylim(0,7) +
+  xlim(0,2.5) +
+  ylim(0,2.5) +
   geom_abline(intercept = 0, # add diagnal line
               slope = c(0.5,2),
               col = "grey",
@@ -508,9 +620,57 @@ p_scatter_manag_sum <- out_reorg_pos %>%
               lty = 'dashed') +
   xlab('Reassembly [sum]\n [Z-score]') + 
   ylab('Restructure [sum]\n [Z-score]') +
-  theme(legend.position = 'bottom') +
-  theme(aspect.ratio=1) # make plots perfect square
+  theme_update(legend.position = 'bottom') +
+  theme_update(aspect.ratio=1) # make plots perfect square
 
+
+
+# Add labels to points, and dominant species
+  
+# add hull polygons
+hull_data <- 
+    out_reorg_pos %>%
+    select(trip_n, manag, RS_mean, RA_mean) %>% 
+    left_join(trip_species) %>%
+    group_by(dom_sp, manag) %>%
+  mutate(dom_sp = factor(dom_sp, # change order of dom_sp
+                         level = c('spruce','beech', 'oak', 'pine'))) %>% 
+    slice(chull(RA_mean, RS_mean)) 
+
+  
+windows()
+#p_scatter_manag_sum <- 
+  out_reorg_pos %>% 
+  left_join(trip_species) %>% 
+  mutate(dom_sp = factor(dom_sp, # change order of dom_sp
+                         level = c('spruce','beech', 'oak', 'pine'))) %>% 
+  ggplot(aes(x = RA_mean,
+             y = RS_mean,
+             #shape = dom_sp,
+             color = dom_sp
+  )) +
+   # geom_text_repel(aes(label = trip_n)) +
+   scale_color_viridis_d(direction = -1) +
+    scale_fill_viridis_d(direction = -1) +
+    geom_polygon(data = hull_data,
+                 aes(fill = dom_sp,
+                     color = dom_sp),
+                 alpha = 0.3,
+                 show.legend = TRUE) +
+    geom_point() +
+ 
+  xlim(0,2.5) +
+  ylim(0,2.5) +
+  geom_abline(intercept = 0, # add diagonal line
+              slope = c(0.5,2),
+              col = "grey",
+              size = .5,
+              lty = 'dashed') +
+    facet_grid(.~manag) +
+  xlab('Reassembly [mean]\n [Z-score]') + 
+  ylab('Restructure [mean]\n [Z-score]') +
+  theme_update(legend.position = 'bottom') +
+  theme_update(aspect.ratio=1) # make plots perfect square
 
 
 
